d277fd5ae3f86fe7c887029ddccd18832b3a5d0c
Nguyen Ba Thanh <thanhnb1101@gmail.com>
2016-04-06 10:49:26 +0700

    add asset folder + html internal file

--- app/src/main/java/thanhnotes/com/notetaker/modules/webservice/WebserviceModuleActivity.java
+++ app/src/main/java/thanhnotes/com/notetaker/modules/webservice/WebserviceModuleActivity.java
@@ -2,17 +2,29 @@ package thanhnotes.com.notetaker.modules.webservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import thanhnotes.com.notetaker.R;
+import thanhnotes.com.notetaker.adapters.ListNewsAdapter;
+import thanhnotes.com.notetaker.models.NewsItem;
 
 /**
  * Created by nguyenbathanh on 4/5/16.
  */
 public class WebserviceModuleActivity extends Activity {
+    private List<NewsItem> listData;
+    private ListView lvNews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.module_webservice_layout);
+
+
+        lvNews = (ListView) findViewById(R.id.lv_news);
+        ListNewsAdapter listNewsAdapter = new ListNewsAdapter(listData, this);
+        lvNews.setAdapter(listNewsAdapter);
 
     }
 }
